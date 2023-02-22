42d8c5407f206ab864356aa77398f4fa445d43c2
MAKGIS <mak-gis2@ukr.net>
2023-02-22 23:49:46 +0200

    comm 20230222 02

--- src/app/modules/shop/components/product-tabs/product-tabs.component.ts
+++ src/app/modules/shop/components/product-tabs/product-tabs.component.ts
@@ -4,6 +4,8 @@ import { specification } from '../../../../../data/shop-product-spec';
 import { reviews } from '../../../../../data/shop-product-reviews';
 import { Review } from '../../../../shared/interfaces/review';
 
+import { PimalionCloudService } from 'src/app/shared/api/pimalion-cloud.service';
+
 @Component({
     selector: 'app-product-tabs',
     templateUrl: './product-tabs.component.html',
@@ -15,12 +17,20 @@ export class ProductTabsComponent {
 
     @Input() product: Product;
 
+
     specification: ProductFeaturesSection[] = specification;
     reviews: Review[] = reviews;
 
-    constructor() { }
+    constructor(private pimalionCloudService: PimalionCloudService
+        ) { }
 
     getAttributes(groupName: string) {
         return this.product.attributesPimalion.filter(data =>  data.groupName === groupName);
     }
+
+    getTitre() {
+        this.pimalionCloudService.getProductDetailPage_01(this.product.id)
+          .subscribe({next:(data: any) => this.product.description = data.description}); // ???
+    }
+
 }
